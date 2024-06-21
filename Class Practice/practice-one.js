779ad850f30561368ea8f2ba3060894b5ef2dccb
heybelen <belendd2312@gmail.com>
2024-06-20 22:11:07 -0700

    Add files via upload

---
+++ Class Practice/practice-one.js
@@ -0,0 +1,21 @@
+
+let exampleObject = {
+    id: 1,
+    name: "Belen"
+}
+
+
+console.log(exampleObject)
+console.log("My name is " + exampleObject.name + ".");
+
+
+function doSomething(){
+    let funcVar = 10;
+    for(let i = 1; i < funcVar + 1; i++){
+        console.log(i);
+
+    }
+}
+
+
+doSomething();
